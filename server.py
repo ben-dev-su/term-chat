56076edd89341425f515a7bb6dcbe2e82a09dfb1
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-01-27 14:49:25 +0100

    add basic multiple client handeling

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
 import socket
+import threading
 from constants import HOST, PORT
 
 
 class Server:
     def __init__(self) -> None:
+        self.__clients = []
         return None
+
+    def handle_clients(self) -> None:
+        return None
+
+    def handle_client_connection(self, client_conn, client_addr):
+        print("Connection from: ", str(client_addr))
+        while True:
+            data = client_conn.recv(1024).decode()
+            if not data:
+                print(f"Connection from: {str(client_addr)} closed.")
+                break
+
+            print("from connected user: ", data)
+            for client_socket in self.__clients:
+                _ = client_socket.send(data.encode())
+            # data = input("> ")
+
+        client_conn.close()
 
     def start(self) -> None:
         with socket.socket() as stream:
@@ -17,17 +37,14 @@ class Server:
                 print(e)
 
             while True:
-                conn, addr = stream.accept()
-                print("Connection from: ", str(addr))
+                client_socket, client_addr = stream.accept()
+                self.__clients.append(client_socket)
+                client_thread = threading.Thread(
+                    target=self.handle_client_connection,
+                    args=(client_socket, client_addr),
+                )
+                client_thread.start()
 
-                while True:
-                    data = conn.recv(1024).decode()
-                    if not data:
-                        print(f"Connection from: {str(addr)} closed.")
-                        break
 
-                    print("from connected user: ", data)
-                    data = input("> ")
-                    _ = conn.send(data.encode())
-
-                conn.close()
+if __name__ == "__main__":
+    _ = Server().start()
