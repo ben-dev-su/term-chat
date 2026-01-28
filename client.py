408243e0bb18339dd84715a7a8e735be49df8428
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-01-28 13:39:00 +0100

    save current work

--- client.py
+++ client.py
@@ -1,16 +1,35 @@
 import socket
+import os
 import threading
 from constants import HOST, PORT
 
 
 class Client:
     def __init__(self) -> None:
+        self.__from_server_client_messages = []
+        self.__user_name = ""
         return None
 
     def recv_messages(self, client_socket) -> None:
         while True:
             data = client_socket.recv(1024).decode()
-            print(data)
+
+            if data:
+                self.__from_server_client_messages.append(data)
+                # print(data)
+                os.system("clear")
+                for msg_rec in self.__from_server_client_messages:
+                    print(msg_rec)
+                # print("> ")
+
+    def handle_input(self, client_socket):
+        try:
+            while True:
+                message = input()
+                if message:
+                    client_socket.send(f"{self.__user_name}: {message}".encode())
+        except OSError as os_error:
+            print(f"Conncetion lost {os_error}")
 
     def start(self) -> None:
         with socket.socket() as client_socket:
@@ -21,16 +40,21 @@ class Client:
             except Exception as e:
                 print(e)
 
+            self.__user_name = input("Gebe deinen Name ein: ")
+
+            client_socket.send(self.__user_name.encode())
+
             client_thread = threading.Thread(
                 target=self.recv_messages, args=(client_socket,)
             )
+
+            input_thread = threading.Thread(
+                target=self.handle_input, args=(client_socket,)
+            )
+            input_thread.start()
             client_thread.start()
-
-            while True:
-                message = input("> ")
-                client_socket.sendall(message.encode())
-
-            client_socket.socket.close()
+            input_thread.join()
+            client_thread.join()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -6,44 +6,49 @@ from constants import HOST, PORT
 class Server:
     def __init__(self) -> None:
         self.__clients = []
+        self.__client_messages = []
         return None
 
     def handle_clients(self) -> None:
         return None
 
     def handle_client_connection(self, client_conn, client_addr):
-        print("Connection from: ", str(client_addr))
         while True:
-            data = client_conn.recv(1024).decode()
-            if not data:
-                print(f"Connection from: {str(client_addr)} closed.")
+            received_message = client_conn.recv(1024).decode()
+            if not received_message:
+                print(f"Connection from: {str(client_addr)} closed")
+                # TODO remove client for __clients
                 break
 
-            print("from connected user: ", data)
+            print(f"[{client_addr}]: ", received_message)
             for client_socket in self.__clients:
-                _ = client_socket.send(data.encode())
-            # data = input("> ")
+                client_socket.send(received_message.encode())
 
         client_conn.close()
 
     def start(self) -> None:
         with socket.socket() as stream:
             try:
-                print(f"try binding socket {HOST} on {PORT}")
                 stream.bind((HOST, PORT))
-                stream.listen()
-                print(f"listening on {HOST} on {PORT}")
+                stream.listen(5)
+                print(f"Server running on {HOST}:{PORT}")
+
+                while True:
+                    print("Waiting for connection...")
+                    client_socket, client_addr = stream.accept()
+                    print(f"Connected to client: {client_addr}")
+
+                    self.__clients.append(client_socket)
+                    client_thread = threading.Thread(
+                        target=self.handle_client_connection,
+                        args=(client_socket, client_addr),
+                    )
+
+                    client_thread.start()
+                    client_thread.join()
+
             except Exception as e:
                 print(e)
-
-            while True:
-                client_socket, client_addr = stream.accept()
-                self.__clients.append(client_socket)
-                client_thread = threading.Thread(
-                    target=self.handle_client_connection,
-                    args=(client_socket, client_addr),
-                )
-                client_thread.start()
 
 
 if __name__ == "__main__":
